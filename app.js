fc356b6da70ddade07cea4bfbb194a1f61689e71
yanbab <yanbab@gmail.com>
2013-04-06 01:56:16 +0200

    Load apps from angular scope

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 /**
  * 
  */
+
+function appCtrl($scope) {
+    $scope.apps = [
+        { name: 'twitter', title: 'Twitter', url: 'http://mobile.twitter.com', icon: 'http://mobile.twitter.com/favicon.ico'},
+        { name: 'facebook', title: 'Facebook', url: 'http://www.facebook.com/mobile/', icon: 'http://www.facebook.com/favicon.ico'},
+        { name: 'soundcloud', title: 'SoundCloud', url: 'http://m.soundcloud.com', icon: 'http://m.soundcloud.com/favicon.ico'}
+    ];
+}
