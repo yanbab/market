bfb18c158eaae3e4eed3837b69249ad1750eb65e
yanbab <yanbab@gmail.com>
2013-04-06 02:26:13 +0200

    Load data from file

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
  * 
  */
 
-function appCtrl($scope) {
-    $scope.apps = [
-        { name: 'twitter', title: 'Twitter', url: 'http://mobile.twitter.com', icon: 'http://mobile.twitter.com/favicon.ico'},
-        { name: 'facebook', title: 'Facebook', url: 'http://www.facebook.com/mobile/', icon: 'http://www.facebook.com/favicon.ico'},
-        { name: 'soundcloud', title: 'SoundCloud', url: 'http://m.soundcloud.com', icon: 'http://m.soundcloud.com/favicon.ico'}
-    ];
+function appCtrl($scope, $http) {
+
+    $http.get('api/apps.json')
+        .success(function(data) {
+            $scope.apps = data;
+        })
+        .error(function() { 
+            $scope.error = true; 
+        }
+    );
+
 }
